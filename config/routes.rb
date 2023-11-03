7ffab05ee27c75807f055d8247101b7ea1d6f119
Hipjea <pierre.duverneix@gmail.com>
2023-11-03 16:48:50 +0100

    fix devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: {
-    sessions: "users/sessions"
+    sessions: "users/sessions",
+    registrations: "users/registrations",
   }
 
   namespace :api do
