e0778af2c9b746c3d7732c762651fc2134897d8d
Hipjea <pierre.duverneix@gmail.com>
2023-11-06 17:53:11 +0100

    fix controller

--- app/controllers/oauth_applications_controller.rb
+++ app/controllers/oauth_applications_controller.rb
@@ -58,6 +58,6 @@ class OauthApplicationsController < ApplicationController
     end
 
     def application_params
-      params.require(:doorkeeper_application).permit(:name, :redirect_uri, :scopes, :confidential)
+      params.require(:custom_application).permit(:name, :redirect_uri, :scopes, :confidential)
     end
 end
