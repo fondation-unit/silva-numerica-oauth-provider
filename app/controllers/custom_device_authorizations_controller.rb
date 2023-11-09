efef7c5fe209383e6c82896f56fa55967754c9fb
Hipjea <pierre.duverneix@gmail.com>
2023-11-09 17:06:41 +0100

    layout

--- app/controllers/custom_device_authorizations_controller.rb
+++ app/controllers/custom_device_authorizations_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class CustomDeviceAuthorizationsController < Doorkeeper::DeviceAuthorizationGrant::DeviceAuthorizationsController
-  layout "doorkeeper"
+  layout "authorize"
 end
